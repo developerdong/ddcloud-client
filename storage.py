c75555e548e369a9e88bbd85a50937a15ca66ec0
developerdong <developerdong@qq.com>
2016-10-03 15:12:21 +0800

    给所有请求加上token

--- storage.py
+++ storage.py
@@ -4,9 +4,10 @@ import requests
 
 
 class Storage:
-    def __init__(self, workDir, server):
+    def __init__(self, workDir, server, token):
         self.workDir = workDir
         self.server = server
+        self.token = token
 
     def getAbsolutePath(self, path):
         os.path.join(self.workDir, path)
@@ -23,16 +24,18 @@ class Storage:
 
     def mkdir(self, dirPath):
         response = requests.post(self.server + '/file/createDir',
-                                 {'dirPath': self.getAbsolutePath(dirPath)})
+                                 {'dirPath': self.getAbsolutePath(dirPath), 'token': self.token})
         return response.json()
 
     def ls(self, dirPath):
-        response = requests.post(self.server + '/file/list', {'dirPath': self.getAbsolutePath(dirPath)})
+        response = requests.post(self.server + '/file/list',
+                                 {'dirPath': self.getAbsolutePath(dirPath), 'token': self.token})
         return response.json()
 
     def upload(self, localFilePath, destDirPath):
         if os.path.exists(localFilePath) and not os.path.isdir(localFilePath):
-            response = requests.post(self.server + '/file/upload', {'destDirPath': self.getAbsolutePath(destDirPath)},
+            response = requests.post(self.server + '/file/upload',
+                                     {'destDirPath': self.getAbsolutePath(destDirPath), 'token': self.token},
                                      files={'file': open(localFilePath, 'rb')})
             return response.json()
         else:
@@ -40,7 +43,8 @@ class Storage:
 
     def download(self, localFilePath, filePath):
         if not os.path.exists(localFilePath):
-            response = requests.post(self.server + '/file/download', {'filePath': self.getAbsolutePath(filePath)})
+            response = requests.post(self.server + '/file/download',
+                                     {'filePath': self.getAbsolutePath(filePath), 'token': self.token})
             if str(response.headers['Content-Type']).find('application/json') == -1:
                 with open(localFilePath, 'wb') as f:
                     f.write(response.content)
