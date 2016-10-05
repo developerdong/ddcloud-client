61be933501f9d610bcf4b9b376de6b7b08c7eb3d
developerdong <developerdong@qq.com>
2016-10-05 13:41:08 +0800

    完成ls分支

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import math
+
+from datetime import datetime
+from prettytable import PrettyTable
+
 from ServerInterface import ServerInterface
+
+
+def convertBytes(length, lst=('Bytes', 'KB', 'MB', 'GB', 'TB', 'PB')):
+    index = 0 if length == 0 else int(math.floor(math.log(length, 1024)))
+    if index >= len(lst):
+        index = len(lst) - 1
+    return ('%.2f' + " " + lst[index]) % (length / math.pow(1024, index))
 
 
 def printHelp(cmd=None):
@@ -34,7 +46,20 @@ interface = ServerInterface(INIT_WORK_DIR, SERVER)
 while True:
     args = str(input()).split()
     if args[0] == 'ls':
-        result = interface.ls(args[1])
+        if len(args) == 1 or len(args) == 2:
+            result = interface.ls(args[1] if len(args) == 2 else None)
+            if result is not None:
+                if result['status'] == 200:
+                    fileTable = PrettyTable(['序号', '名称', '大小', '目录', '修改时间', '访问时间'])
+                    for i, row in enumerate(result['result']):
+                        fileTable.add_row([i, row['name'], convertBytes(row['length']), row['dir'],
+                                           datetime.fromtimestamp(row['modificationTime'] / 1000),
+                                           datetime.fromtimestamp(row['accessTime'] / 1000)])
+                    print(fileTable)
+                else:
+                    print(result['result'])
+        else:
+            printHelp('ls')
     elif args[0] == 'cd':
         result = interface.cd(args[1])
     elif args[0] == 'mkdir':
